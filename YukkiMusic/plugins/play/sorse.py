2e3c0338120a98dca09fc659e04e14e7c538a6c5
JOKER <115378681+WYYYRR@users.noreply.github.com>
2023-01-27 16:22:07 +0300

    Add files via upload

--- YukkiMusic/plugins/play/sorse.py
+++ YukkiMusic/plugins/play/sorse.py
@@ -19,19 +19,19 @@ from YukkiMusic import app
 )
 async def khalid(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/185ff2702adb51c3cd1e9.jpg",
-        caption=f"""╭──★── • ◈ • ──★──╮\n么 [𝐒𝐎𝐔𝐑𝐂𝐄 FoX](https://t.me/L9L9XX)\n么 [BoT Tws](https://t.me/Diller1_bot)\n么 [HuSSoN](https://t.me/TTTT_TB)\n么 [ExP EvaN](https://t.me/EVAN_BOTT)\n╰──★── • ◈ • ──★──╯\n[⍟ 𝚆𝙴𝙻𝙲𝙾𝙼 𝚃𝙾 𝚂𝙾𝚄𝚁𝙲𝙴 FoX](https://t.me/L9L9XX)""",
+        photo=f"https://telegra.ph/file/2b37b74ac8a1b30c2d00a.jpg",
+        caption=f"""╭──★── • ◈ • ──★──╮\n么 [𝐒𝐎𝐔𝐑𝐂𝐄 𝐕𝐄𝐆𝐀](https://t.me/VEGA_source)\n么 [𝐀𝐒𝗞 𝗧𝐎 𝐌𝗘](https://t.me/VEGA_source)\n么 [𝐊𝐈𝐌𝐌𝐘](https://t.me/KIMMY50)\n么 [𝐙𝐎𝐌𝐁𝐈𝐄](https://t.me/Zo_Mbi_e)\n╰──★── • ◈ • ──★──╯\n[⍟ 𝚆𝙴𝙻𝙲𝙾𝙼 𝚃𝙾 𝚂𝙾𝚄𝚁𝙲𝙴 𝚅𝙴𝙶𝙰](https://t.me/VEGA_source)""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⌯ 𝗛𝘂𝘀𝘀𝗮𝗶𝗻 ☬ ", url=f"https://t.me/TTTT_TB"), 
+                        "⸙⃟꯭𝗞᳙𝗜᳤᳝𝗠᳜᳜᳖᳗᳘᳙᳚᳚᳚᳚᳚𝗠᳙𝗬᳧᳗♱𝙓⃟⃟⃟⃟⃟⃟‌🇩🇪⁖℘➸ 𝘼ٍ𝙇ٍ 𝟯𝘼ٍ𝙎𝘼ٍ𝘽ٍ𝘼☬", url=f"https://t.me/KIMMY50"), 
                 ],[
                     InlineKeyboardButton(
-                        "Team FoX", url=f"https://t.me/L9L9XX"),
+                        "𝐒𝐎𝐔𝐑𝐂𝐑 𝐕𝐄𝐆𝐀", url=f"https://t.me/VEGA_source"),
                 ],[
                     InlineKeyboardButton(
-                        "اضغط لاضافه البوت لمجموعتك✅.", url=f"https://t.me/S9SSS_BOT?startgroup=true"),
+                        "اضغط لاضافه البوت لمجموعتك✅.", url=f"https://t.me/KiMY_X_bot?startgroup=true"),
                 ],
 
             ]
